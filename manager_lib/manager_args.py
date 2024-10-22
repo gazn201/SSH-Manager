d01e64f8b872f043841219bbd2c8f2cd37eb2519
Nemchenko Denys Dmytrovych <denys.nemchenko@infotech.gov.ua>
2024-10-22 16:25:12 +0300

    Upgrading dotenv implementing

--- manager_lib/manager_args.py
+++ manager_lib/manager_args.py
@@ -1,10 +1,18 @@
 import os
 from dotenv import load_dotenv
 
-
 DOTENV_PATH = os.path.expanduser("~/.ssh-manager/.env")
-load_dotenv(dotenv_path=DOTENV_PATH)
 
-SCRIPT_HOME = os.getenv('SCRIPT_HOME')
-SSH_CONFIG = os.getenv('SSH_CONFIG')
-DATABASE_PATH = os.getenv('DATABASE_PATH')
+if os.path.exists()
+    load_dotenv(dotenv_path=DOTENV_PATH)
+    
+    SCRIPT_HOME = os.getenv('SCRIPT_HOME')
+    SSH_CONFIG = os.getenv('SSH_CONFIG')
+    DATABASE_PATH = os.getenv('DATABASE_PATH')
+
+else:
+    SCRIPT_HOME = os.path.expanduser("~/.ssh-manager")
+    SSH_CONFIG = os.path.expanduser("~/.ssh/config")
+    DATABASE_PATH = os.path.expanduser("/.ssh-manager/ssh-conf.db")
+
+   
